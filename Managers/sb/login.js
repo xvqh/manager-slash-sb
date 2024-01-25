89e51fb00751e887a68030f10b19df13158d51d1
xvqh <138306559+sewiiiii@users.noreply.github.com>
2024-01-25 18:15:49 +0100

    Update login.js

--- Managers/sb/login.js
+++ Managers/sb/login.js
@@ -28,7 +28,7 @@ module.exports = {
             console.log('UserTag:', userTag);
             console.log('Token:', token);
 
-            const client = new Client({ checkUpdate: false, autoRedeemNitro: false, ws: { properties: { os: 'Linux', browser: 'Discord Client', release_channel: 'stable', client_version: '1.0.9011', os_version: '10.0.22621', os_arch: 'x64', system_locale: 'en-US', client_build_number: 175517, native_build_number: 29584, client_event_source: null, design_id: 0, } } });
+            const client = new Client({ checkUpdate: false, autoRedeemNitro: false});
 
             client.snipes = new Map();
             client.commands = new Collection();
